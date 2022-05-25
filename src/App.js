aed0992b9bfddb8dbe44bd18602f6192f17b060b
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-05-25 19:33:37 +0530

    restaurant details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Filter from './components/Filter';
+import Navbar from './components/Navbar';
+import Offer from './components/Offer';
+import Restaurants from './components/Restaurants';
+import userinfo from './data/userinfo.json';
+import restaurants from './data/restaurants.json'
+import offers from './data/offers.json'
+import { useState } from 'react';
 
 function App() {
+  const [filterBy, setFilterBy] = useState("");
+  const [data, setData] = useState([]);
+  const updateFilter = (newFilter) => {
+    // some code will be written here
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar {...userinfo.location}/>
+      <Offer offers={offers} />
+      <section className='near-you'>
+        <Filter currentFilteredBy={filterBy} updateFilter={updateFilter} />
+        <Restaurants restaurants={restaurants}/>
+      </section>
     </div>
   );
 }
